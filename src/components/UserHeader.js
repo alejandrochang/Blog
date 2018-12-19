63f161429b3bd0c01bd5482f629a4c707b921422
Alejandro <alejandrochang10@gmail.com>
2018-12-19 15:01:26 -0800

    adding users reducer to combineReducers

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -3,10 +3,12 @@ import { connect } from 'react-redux';
 import { fetchUser } from '../actions';
 
 class UserHeader extends Component {
+  
   componentDidMount() {
     this.props.fetchUser(this.props.userId);
   }
   render() {
+    console.log('props', this.props);
     return(
       <div>UserHeader</div>
     );
