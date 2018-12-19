f91838cc311824e2ff84b13722ef2284d94f1a8e
Alejandro <alejandrochang10@gmail.com>
2018-12-19 15:23:41 -0800

    destructuring user

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -7,20 +7,20 @@ class UserHeader extends Component {
     this.props.fetchUser(this.props.userId);
   }
   render() {
-    console.log('user', this.props.user)
-    if (!this.props.user) {
+    const { user } = this.props;
+    if (!user) {
       return null;
     }
     return(
-      <div className="header">{ this.props.user.name }</div>
+      <div className="header">{ user.name }</div>
     );
   }
 }
 
-const mapStateToPrps = (state, ownProps) => {
+const mapStateToProps = (state, ownProps) => {
   return {
     user: state.users.find((user) => user.id === ownProps.userId)
   }
 }
 
-export default connect(mapStateToPrps, { fetchUser })(UserHeader);
+export default connect(mapStateToProps, { fetchUser })(UserHeader);
