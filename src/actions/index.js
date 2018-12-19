336135ca3c3585e2b11826554b8c7f7a0180d8a4
Alejandro <alejandrochang10@gmail.com>
2018-12-18 23:22:21 -0800

    action creator for fetching a single user

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,10 +6,10 @@ export const fetchPosts = () => async dispatch => {
     dispatch({ type: 'FETCH_POSTS', payload: response.data })
 };
 
-export const fetchUser = () => async dispatch => {
-  const response = await jsonPlaceholder.get('/users/1');
-  
-  dispatch({ type: 'FETCH_USER', payload: response })
+export const fetchUser = (id) => async dispatch => {
+  const response = await jsonPlaceholder.get(`/users/${id}`);
+
+  dispatch({ type: 'FETCH_USER', payload: response.data })
 }
 
 /* Regular function (less condensed)
