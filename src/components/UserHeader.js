ec5054802e7256888100081f6744b08afff357b1
Alejandro <alejandrochang10@gmail.com>
2018-12-19 16:34:59 -0800

    updating fetchPostsAndUser to clearer code

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -1,26 +1,20 @@
-import React, {Component} from 'react';
-import { connect } from 'react-redux';
-import { fetchUser } from '../actions';
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
 class UserHeader extends Component {
-  componentDidMount() {
-    this.props.fetchUser(this.props.userId);
-  }
   render() {
     const { user } = this.props;
     if (!user) {
       return null;
     }
-    return(
-      <div className="header">{ user.name }</div>
-    );
+    return <div className="header">{user.name}</div>;
   }
 }
 
 const mapStateToProps = (state, ownProps) => {
   return {
-    user: state.users.find((user) => user.id === ownProps.userId)
-  }
-}
+    user: state.users.find(user => user.id === ownProps.userId)
+  };
+};
 
-export default connect(mapStateToProps, { fetchUser })(UserHeader);
+export default connect(mapStateToProps)(UserHeader);
